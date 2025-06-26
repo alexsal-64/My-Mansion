cde9025a72ce335fc04ef420149587ab21c1f974
alexsal-64 <131720227+alexsal-64@users.noreply.github.com>
2025-06-26 18:47:21 +0000

    Update scene_manager.h

--- include/scene_manager.h
+++ include/scene_manager.h
@@ -1,1 +1,17 @@
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
 
+typedef enum {
+    SCENE_TRANSITION,
+    SCENE_MENU,
+    // Agrega aquí más escenas en el futuro
+} SceneID;
+
+void SceneManager_Init(void);
+void SceneManager_Update(void);
+void SceneManager_Draw(void);
+void SceneManager_Unload(void);
+void SceneManager_Change(SceneID newScene);
+SceneID SceneManager_GetCurrent(void);
+
+#endif // SCENE_MANAGER_H
