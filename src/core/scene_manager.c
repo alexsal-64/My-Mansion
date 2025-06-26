96eb89f567c67d15e85ed9d0d73f85c9347387d4
alexsal-64 <131720227+alexsal-64@users.noreply.github.com>
2025-06-26 18:49:35 +0000

    Update scene_manager.c

--- src/core/scene_manager.c
+++ src/core/scene_manager.c
@@ -1,1 +1,51 @@
+#include "scene_manager.h"
+#include "../effects/transition_circle.h"
+#include "../scenes/scene_menu.h"
 
+static SceneID currentScene;
+
+void SceneManager_Init(void) {
+    currentScene = SCENE_TRANSITION;
+    TransitionCircle_Init();
+}
+
+void SceneManager_Update(void) {
+    switch (currentScene) {
+        case SCENE_TRANSITION:
+            TransitionCircle_Update();
+            if (TransitionCircle_IsDone()) {
+                SceneManager_Change(SCENE_MENU);
+            }
+            break;
+        case SCENE_MENU:
+            SceneMenu_Update();
+            break;
+    }
+}
+
+void SceneManager_Draw(void) {
+    switch (currentScene) {
+        case SCENE_TRANSITION:
+            TransitionCircle_Draw();
+            break;
+        case SCENE_MENU:
+            SceneMenu_Draw();
+            break;
+    }
+}
+
+void SceneManager_Unload(void) {
+    // Placeholder para liberar recursos si es necesario
+}
+
+void SceneManager_Change(SceneID newScene) {
+    currentScene = newScene;
+    switch (newScene) {
+        case SCENE_TRANSITION: TransitionCircle_Init(); break;
+        case SCENE_MENU: SceneMenu_Init(); break;
+    }
+}
+
+SceneID SceneManager_GetCurrent(void) {
+    return currentScene;
+}
