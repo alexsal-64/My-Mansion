2568350675631028340084a30275edea54cd6ec4
alexsal-64 <131720227+alexsal-64@users.noreply.github.com>
2025-07-13 18:44:29 +0000

    Create scene_select.h

---
+++ include/scenes/scene_select.h
@@ -0,0 +1,13 @@
+#ifndef SCENE_SELECT_H
+#define SCENE_SELECT_H
+
+// Inicializa la escena del menú principal
+void SceneSelect_Init(void);
+
+// Actualiza la lógica del menú
+void SceneSelect_Update(void);
+
+// Dibuja el menú principal
+void SceneSelect_Draw(void);
+
+#endif // SCENE_SELECT_H
